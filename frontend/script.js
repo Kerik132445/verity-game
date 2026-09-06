6fdb249e32b470638a42092651f4179dfa32476c
kiryha <kerik1pii@gmail.com>
2026-09-06 17:19:50 +0300

    Добавил инвентарь и пару предметов

--- frontend/script.js
+++ frontend/script.js
@@ -3,6 +3,13 @@ console.log("SCRIPT JS ЗАГРУЗИЛСЯ");
 let messagesLeft = 3
 let isWaitingForReply = false
 
+let inventory = []
+
+inventory.push("coin")
+inventory.push("key")
+inventory.push("cassette")
+inventory.push("note")
+
 function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -139,8 +146,6 @@ async function sendMessage() {
 
 		input.focus();
 	}
-
-	console.log(data);
 }
 
 document.getElementById("sendButton").addEventListener("click", sendMessage)
@@ -152,3 +157,95 @@ document.getElementById("messageInput").addEventListener("keydown", (event) => {
 })
 
 document.getElementById("adButton").addEventListener("click", rewardAd)
+
+
+
+
+
+const inventoryButton = document.getElementById("inventoryButton")
+const inventoryModal = document.getElementById("inventoryModal")
+const closeInventory = document.getElementById("closeInventory")
+const inventoryGrid = document.getElementById("inventoryGrid")
+
+function openInventory() {
+	inventoryModal.style.display = "flex"
+	renderInventory()
+}
+
+function closeInventoryWindow() {
+	inventoryModal.style.display = "none"
+}
+
+
+inventoryButton.addEventListener("click", openInventory)
+
+closeInventory.addEventListener("click", closeInventoryWindow)
+
+inventoryModal.addEventListener("click", (event) => {
+	if (event.target == inventoryModal) {
+		closeInventoryWindow()
+	}
+})
+
+
+
+const items = [
+	{
+		id: "coin",
+		name: "Старая монета",
+		image: "images/old_coin.png",
+		description: "Странная старая монета."
+	},
+
+	{
+		id: "key",
+		name: "Маленький ключ",
+		image: "images/key.webp",
+		description: "Неизвестно, что он открывает."
+	},
+
+	{
+		id: "cassette",
+		name: "Кассета",
+		image: "images/cassette.png",
+		description: "Верити почему-то не хочет, чтобы ты её включал."
+	},
+
+	{
+		id: "note",
+		name: "Старая записка",
+		image: "images/note.webp",
+		description: "Верити почему-то не хочет, чтобы ты её прочитал."
+	},
+]
+
+function renderInventory() {
+	inventoryGrid.innerHTML = ""
+
+	for (let i = 0; i < 15; i++) {
+		const slot = document.createElement("div")
+
+		slot.classList.add("inventory-slot")
+
+		const itemId = inventory[i]
+
+		if (itemId) {
+			const item = items.find(item => item.id === itemId)
+
+			if (item) {
+				const image = document.createElement("img")
+
+				image.src = item.image
+				image.alt = item.name
+
+				image.classList.add("inventory-item")
+
+				slot.appendChild(image)
+
+				slot.title = item.name
+			}
+		}
+
+		inventoryGrid.append(slot)
+	}
+}
